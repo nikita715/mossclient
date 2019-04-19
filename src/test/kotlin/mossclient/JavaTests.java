110676bfb5d6919fa070cf4470dff7b228058482
Nikita Stepochkin <nikstepbox@gmail.com>
2019-04-19 22:19:14 +0300

    test: add more tests

--- src/test/kotlin/mossclient/JavaTests.java
+++ src/test/kotlin/mossclient/JavaTests.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertThat;
 
@@ -17,12 +17,13 @@ public class JavaTests {
     private String baseDir = JavaTests.class.getClassLoader().getResource("basefiles").getFile();
     private String solutionDir = JavaTests.class.getClassLoader().getResource("solutionfiles").getFile();
 
-    private List<File> bases = Stream.of(1, 2).map(i -> new File(baseDir + "/file" + i + ".txt")).collect(Collectors.toList());
+    private List<File> bases = Stream.of(1, 2).map(i -> new File(baseDir + "/file" + i + ".txt")).collect(toList());
     private List<Pair<String, File>> solutions = Stream.iterate(1, i -> i + 1).limit(29)
-            .map(i -> new Pair<>("student" + i, new File(solutionDir + "/file" + i + ".txt"))).collect(Collectors.toList());
+            .map(i -> new Pair<>("student" + i, new File(solutionDir + "/file" + i + ".txt"))).collect(toList());
+    private String mossId = System.getenv("MOSS_ID");
 
     @Test
-    public void smallUploadTest() {
+    public void sampleUpload() {
         List<File> bases = Arrays.asList(
                 new File(baseDir + "/file1.txt"),
                 new File(baseDir + "/file2.txt")
@@ -34,7 +35,7 @@ public class JavaTests {
                 new Pair<>("student3", new File(solutionDir + "/file3.txt"))
         );
 
-        try (MossClient mossClient = new MossClient(System.getenv("MOSS_ID"), MossLanguage.JAVA)) {
+        try (MossClient mossClient = new MossClient(mossId, MossLanguage.JAVA)) {
             mossClient.submitFiles(bases, true);
             mossClient.submitNamedFiles(solutions);
             String resultUrl = mossClient.getResult();
@@ -42,14 +43,42 @@ public class JavaTests {
             assertThat(resultUrl, startsWith("http://moss.stanford.edu/results/"));
             System.out.println(resultUrl);
         }
-
     }
 
     @Test
-    public void bigUploadTest() {
-        String resultUrl = new MossClient(System.getenv("MOSS_ID"), MossLanguage.JAVA)
-                .experimental().resultSize(5).maxMatches(10)
-                .submitFiles(bases, true).submitNamedFiles(solutions).getResult();
+    public void uploadFiles() {
+        List<File> bases = Arrays.asList(
+                new File(baseDir + "/file1.txt"),
+                new File(baseDir + "/file2.txt")
+        );
+
+        List<File> solutions = Arrays.asList(
+                new File(solutionDir + "/file1.txt"),
+                new File(solutionDir + "/file2.txt"),
+                new File(solutionDir + "/file3.txt")
+        );
+
+        try (MossClient mossClient = new MossClient(mossId, MossLanguage.JAVA)) {
+            mossClient.submitFiles(bases, true);
+            mossClient.submitFiles(solutions);
+            String resultUrl = mossClient.getResult();
+
+            assertThat(resultUrl, startsWith("http://moss.stanford.edu/results/"));
+            System.out.println(resultUrl);
+        }
+    }
+
+    @Test
+    public void fullFeaturedUpload() {
+        String resultUrl = new MossClient(mossId, MossLanguage.JAVA)
+                .experimental()
+                .resultSize(5)
+                .maxMatches(10)
+                .directoryBased(false)
+                .comment("important comment")
+                .submitFiles(bases, true)
+                .submitNamedFiles(solutions)
+                .getResult();
         assertThat(resultUrl, startsWith("http://moss.stanford.edu/results/"));
         System.out.println(resultUrl);
     }
--- src/test/kotlin/mossclient/KotlinTests.kt
+++ src/test/kotlin/mossclient/KotlinTests.kt
@@ -8,6 +8,7 @@ import kotlin.test.assertFailsWith
 
 open class KotlinTests {
 
+    private val mossId = System.getenv("MOSS_ID")
     private val baseDir = KotlinTests::class.java.classLoader.getResource("basefiles").file
     private val solutionDir = KotlinTests::class.java.classLoader.getResource("solutionfiles").file
 
@@ -15,12 +16,13 @@ open class KotlinTests {
     private val solutions: List<Pair<String, File>> = (1..29).map { "student$it" to File("$solutionDir/file$it.txt") }
 
     @Test
-    fun bigUploadTest() {
-        val client = MossClient(System.getenv("MOSS_ID"), MossLanguage.JAVA)
+    fun fullFeaturedTest() {
+        val client = MossClient(mossId, MossLanguage.JAVA)
             .submitFiles(bases, true)
             .submitNamedFiles(solutions)
             .rawOptions("-x -m 123")
             .directoryBased(false)
+            .comment("important comment")
             .resultSize(4)
         val resultUrl = client.getResult()
         assertThat(resultUrl, startsWith("http://moss.stanford.edu/results/"))
@@ -29,10 +31,10 @@ open class KotlinTests {
 
     @Test
     fun tryToSubmitTwoTimes() {
-        val resultSize = MossClient(System.getenv("MOSS_ID"), MossLanguage.JAVA)
+        val resultSize = MossClient(mossId, MossLanguage.JAVA)
             .submitFiles(bases, true)
             .submitNamedFiles(solutions)
-        resultSize.getResult()
+        println(resultSize.getResult())
 
         assertFailsWith(MossClientException::class) {
             resultSize.getResult()
@@ -41,7 +43,7 @@ open class KotlinTests {
 
     @Test
     fun tryToPassWrongOptions() {
-        val client = MossClient(System.getenv("MOSS_ID"), MossLanguage.JAVA)
+        val client = MossClient(mossId, MossLanguage.JAVA)
             .submitFiles(bases, true)
             .submitNamedFiles(solutions)
             .directoryBased(false)
@@ -51,8 +53,77 @@ open class KotlinTests {
             client.rawOptions("wrong options")
         }
 
-        assertThat(client.getResult(), startsWith("http://moss.stanford.edu/results/"))
+        val resultUrl = client.getResult()
+        assertThat(resultUrl, startsWith("http://moss.stanford.edu/results/"))
+        println(resultUrl)
     }
 
+    @Test
+    fun submitStringFiles() {
 
+        val client = MossClient(mossId, MossLanguage.JAVA)
+
+        val file1String = """
+public class Range {
+    private int leftB;
+    private int rightB;
+    private Range() {}
+    private Range(int leftB, int rightB){
+            this.leftB = leftB;
+            this.rightB = rightB;
+    }
+}"""
+
+        val file2String = """
+public class Range {
+
+    private int left = 0;
+    private int right = 0;
+
+    public Range(int left, int right) {
+        if(left > right){
+          throw new IllegalArgumentException("WRONG!Lower bound is greater then upper bound");
+        }
+        this.left = left;
+        this.right = right;
+    }
+}"""
+
+        client.submitStringFiles(
+            listOf(
+                "student1" to file1String,
+                "student2" to file2String
+            )
+        )
+
+        val resultUrl = client.getResult()
+        assertThat(resultUrl, startsWith("http://moss.stanford.edu/results/"))
+        println(resultUrl)
+
+    }
+
+    @Test
+    fun submitByteArrayFiles() {
+
+        val client = MossClient(mossId, MossLanguage.JAVA)
+
+        val bytes = listOf<Byte>(
+            10, 112, 117, 98, 108, 105, 99, 32, 99, 108, 97, 115, 115, 32, 82, 97,
+            110, 103, 101, 32, 123, 10, 32, 32, 32, 32, 112, 114, 105, 118, 97, 116, 101, 32, 105, 110,
+            116, 32, 108, 101, 102, 116, 66, 59, 10, 32, 32, 32, 32, 112, 114, 105, 118, 97, 116, 101,
+            32, 105, 110, 116, 32, 114, 105, 103, 104, 116, 66, 59, 10, 32, 32, 32, 32, 112, 114, 105
+        )
+
+        client.submitByteArrayFiles(
+            listOf(
+                "student1" to ByteArray(bytes.size, bytes::get),
+                "student2" to ByteArray(bytes.size, bytes::get)
+            )
+        )
+
+        val resultUrl = client.getResult()
+        assertThat(resultUrl, startsWith("http://moss.stanford.edu/results/"))
+        println(resultUrl)
+
+    }
 }
